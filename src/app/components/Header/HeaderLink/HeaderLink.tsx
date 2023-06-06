1639762ba55668108d1e51b6be8b80fb65ad86a2
Daniil Silin <daniil_silin@mail.ru>
2023-06-06 18:10:51 +0300

    Updated header

--- src/app/components/Header/HeaderLink/HeaderLink.tsx
+++ src/app/components/Header/HeaderLink/HeaderLink.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from "react";
 import styles from "./HeaderLink.module.scss";
 import Link from "next/link";
 
 interface HeaderLinkI {
-    url: string
-    title: string
+  url: string;
+  title: string;
+  setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const HeaderLink: React.FC<HeaderLinkI> = ({url, title}) => {
-    return (
-        <div className={styles.link}>
-            <Link href={`/${url}`}>
-                <span>{title}</span>
-            </Link>
-        </div>
-    );
+const HeaderLink: React.FC<HeaderLinkI> = ({ url, title, setOpen }) => {
+  return (
+    <div className={styles.link}>
+      <Link href={`/${url}`} onClick={() => setOpen(false)}>
+        <span>{title}</span>
+      </Link>
+    </div>
+  );
 };
 
 export default HeaderLink;
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { Monoton } from "next/font/google";
 import HeaderLink from "@/app/components/Header/HeaderLink/HeaderLink";
-import EnglishIcon from "@/app/components/Header/Language/EnglishIcon";
-import RussianIcon from "@/app/components/Header/Language/RussianIcon";
+import Link from "next/link";
 
 const monoton = Monoton({
   weight: "400",
@@ -13,24 +12,47 @@ const monoton = Monoton({
 });
 
 export const Header = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <header className={styles.container}>
-      <div className={`${styles.logo} ${monoton.variable}`}>
-        <span>RI0FS</span>
-      </div>
+      <Link href={"/"}>
+        <div className={`${styles.logo} ${monoton.variable}`}>
+          <span>RI0FS</span>
+        </div>
+      </Link>
 
-      <div className={styles.right}>
+      <div
+        className={`${styles.right} ${
+          isOpen ? styles["right--expanded"] : styles["right--closed"]
+        }`}
+      >
         <nav className={styles.menu}>
           {/* <HeaderLink url={"team"} title={"Команда"} />
           <HeaderLink url={"team"} title={"Техника"} />
           <HeaderLink url={"team"} title={"Расписание"} /> */}
-          <HeaderLink url={"news"} title={"Новости"} />
-          <HeaderLink url={"donate"} title={"Пожертвования"} />
-          <HeaderLink url={"qsl"} title={"QSL"} />
-          <HeaderLink url={"log"} title={"Online log"} />
-          <HeaderLink url={"eng"} title={"English"} />
+          <HeaderLink url={"news"} title={"Новости"} setOpen={setOpen} />
+          <HeaderLink
+            url={"donate"}
+            title={"Пожертвования"}
+            setOpen={setOpen}
+          />
+          <HeaderLink url={"qsl"} title={"QSL"} setOpen={setOpen} />
+          <HeaderLink url={"log"} title={"Online log"} setOpen={setOpen} />
+          <HeaderLink url={"eng"} title={"English"} setOpen={setOpen} />
         </nav>
+        <button
+          className={`${styles["button--inner"]} ${styles.menuButton}`}
+          onClick={() => setOpen(!isOpen)}
+        >
+          Закрыть
+        </button>
       </div>
+      <button
+        className={`${styles["button--outer"]} ${styles.menuButton}`}
+        onClick={() => setOpen(!isOpen)}
+      >
+        Меню
+      </button>
     </header>
   );
 };
